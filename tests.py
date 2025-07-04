54a67abd5ec12c97f0fcaf58ff16f46924e97177
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2025-07-04 15:33:12 +0700

    Implement 'write file' function

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
 def print_result(description, result):
@@ -16,9 +16,12 @@ def print_result(description, result):
 
 if __name__ == "__main__":
     # Test 1: List contents of calculator directory
-    result = get_file_content("calculator", "main.py")
+    result = write_file("calculator", "lorem.txt",
+                        "wait, this isn't lorem ipsum")
     print(result)
-    result = get_file_content("calculator", "pkg/calculator.py")
+    result = write_file("calculator", "pkg/morelorem.txt",
+                        "lorem ipsum dolor sit amet")
     print(result)
-    result = get_file_content("calculator", "/bin/cat")
+    result = write_file("calculator", "/tmp/temp.txt",
+                        "this should not be allowed")
     print(result)
